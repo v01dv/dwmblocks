57f9a298e1776dc346ccfcb0eda563ef679d761d
voldv <gt.en72v@simplelogin.co>
2023-03-17 01:13:20 +0200

    Initial commit.

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,29 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+  /*Icon*/  /*Command*/   /*Update Interval*/ /*Update Signal*/
+  // {"",      "sb-nettraf",       1,  16},
+  // {"",      "sb-internet",      5,  4},
+  // {"",      "sb-music",         0,  11},
+  {"├─",    "",                 0,  0},
+  {"",      "sb-iplocale",      5,  16},
+  {"",      "sb-fan",           5,  17}, 
+  {"",      "sb-tasks",         10, 26},
+  {"",      "sb-cpu",           10, 18}, 
+  {"",      "sb-load-average",  1,  20}, 
+  {"",      "sb-cpubars",       1,  19}, 
+  {"",      "sb-memory",        10, 14}, 
+  {"",      "sb-gputemp",       10, 13}, 
+  {"",      "sb-battery",       5,  3},
+  {"",      "sb-volume",        0,  10},
+  {"",      "sb-clock",         60, 1},
+  {"",      "sb-help-icon",     0,  15},
+  // {"⌨",     "kbselect",         0,  30},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " ";
 static unsigned int delimLen = 5;
+
+// Have dwmblocks automatically recompile and run when you edit this file in
+// vim with the following line in your vimrc/init.vim:
+// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
